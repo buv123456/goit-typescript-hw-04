1a2073d6ec1ab3e741ded8f8e978223469a27615
Volodymyr <buvoro@gmail.com>
2023-10-29 06:52:56 +0200

    done

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -5,11 +5,11 @@ type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 
 // Додати тип Menu Selected
-type SelectedMenu = { id?: MenuIds };
+type SelectedMenu = { id: MenuIds };
 type MenuSelected = { selectedMenu: SelectedMenu };
 
 const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
+  selectedMenu: { id: "first" },
 });
 
 // Додайте тип MenuAction
@@ -26,7 +26,9 @@ type PropsProvider = {
 
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({
+    id: "first",
+  });
 
   const menuContextAction = useMemo(
     () => ({
